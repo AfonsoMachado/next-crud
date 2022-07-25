12092097d7d00dce47e6778fabb9f67adac592a6
Afonso Machado <afonso.machado@blockhub.builders>
2022-07-25 11:43:10 -0300

    render table

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,15 @@
 import Layout from "../components/Layout";
+import Table from "../components/Table";
+import Client from "../core/Client";
 
 export default function Home() {
+  const clients = [
+    new Client("Ana", 34, "1"),
+    new Client("Bia", 23, "2"),
+    new Client("Carlos", 54, "3"),
+    new Client("Pedro", 37, "4"),
+  ];
+
   return (
     <div
       className={`
@@ -10,7 +19,7 @@ export default function Home() {
     `}
     >
       <Layout title="Cadastro Simples">
-        <span>content</span>
+        <Table clients={clients}></Table>
       </Layout>
     </div>
   );
